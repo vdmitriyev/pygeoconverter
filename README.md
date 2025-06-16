277c7449d22aa12d7282cbe41306c2bd605c5e43
Dmitriyev <2291074+vdmitriyev@users.noreply.github.com>
2025-06-16 12:35:08 +0200

    FIX: license and script

--- README.md
+++ README.md
@@ -1,32 +1,32 @@
-### About
+## About
 
 Converting openly accessible address locations  into geographical coordinates (longitude and latitude). Primary the work done with means of OSM. For further details regarding features refer to ```pygeoconverter.py``` script. I was able to download around 55 000 coordinates in 3 days (each day script worked not more then 5-6 hours and was also continuously improved).
 
-**NOTE** Script should skip already downloaded locations. But be careful - interrupting script while working may remove already downloaded data (only from last "batch", which is also configurable). Because of this reason backup in form of 'tar' file is always created before launching retrieving process.
+**NOTE**: Script should skip already converted locations. But be careful - interrupting script while working may remove already converted data (only from last "batch", which is also configurable). Because of this reason backup in form of 'tar' file is always created before launching retrieving process.
 
-### Structure
+## Structure
 
 * **data** - contains initial csv with addresses and resulting data (py dict + CSV)
 * **pygeoconverter** - main Python script + configuration file
 * **experiments** - multiple test of other geo python libraries (e.g. Google Map)
 
-### Dependencies
+## Dependencies
 
 * Python 3
-* geocoder
+* `requirements/requirements-prod.txt`
 
-### Preparing
+## Preparing
 
 * Install dependencies
-	- [scripts/cmdInitiateEnv.bat]
-* Create folder `data` and file `SampleDataSource.csv`
+	- [scripts/cmdInitiateEnv.bat](scripts/cmdInitiateEnv.bat)
+* (optional) create folder `data` and file `SampleDataSource.csv`
 	```
 	mkdir data
 	cd data
 	touch SampleDataSource.csv
 	```	
 
-### Usage
+## Usage
 
 In case there were no data downloaded before script will through exception first time. But on the second run should proceed successfully. For configurations use 'configs.py' file.
 
@@ -37,3 +37,6 @@ cd pygeoconverter
 python pygeoconverter.py
 ```
 
+## License
+
+[MIT]
--- pygeoconverter/pygeoconverter.py
+++ pygeoconverter/pygeoconverter.py
@@ -1,41 +1,36 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__author__ = 'Viktor Dmitriyev'
-__license__ = 'MIT'
-__version__ = '1.0.0'
-__maintainer__ = '-'
-__email__ = ''
-__status__ = 'dev'
-__date__ = '11.03.2016'
-__description__ = 'Converting locations from open access data to the longitude and latitude.'
+__version__ = "1.0.0"
+__date__ = "11.03.2016"
+__description__ = "Converting locations from open access data to the longitude and latitude."
 
 import os
 import sys
-import uuid
-import geocoder
-import configs as cfg
 import tarfile
 import time
-from time import sleep
-from pprint import pprint
+import uuid
 from datetime import datetime
+from pprint import pprint
+from time import sleep
+
+import configs as cfg
+import geocoder
 
 # set 'True' for debugging (stdout will be more verbose)
 DEBUG = False
 VERIFY = True
 
-class GeoConverter():
+
+class GeoConverter:
 
     def __init__(self):
-        """ Init method """
+        """Init method"""
 
-        if DEBUG: print ('[i] class "GeoConverter" created')
+        if DEBUG:
+            print('[i] class "GeoConverter" created')
         self.geo_database = {}
         self.unique_keys = {}
 
     def prepeare_data(self):
-        """ Preparing data for work - loading and making backups"""
+        """Preparing data for work - loading and making backups"""
 
         self.make_db_backup()
 
@@ -51,78 +46,85 @@ class GeoConverter():
         # for key in self.geo_database:
         #     if key in self.unique_keys:
         #         tmp[key] = self.geo_database[key]
-        #pprint(tmp)
+        # pprint(tmp)
 
     def make_db_backup(self):
-        """ Making backup of downloaded db """
+        """Making backup of downloaded db"""
 
         ts = time.time()
-        prefix = '{0}-{1}'.format(datetime.fromtimestamp(ts).strftime('%Y%m%d-%H%M%S'), str(uuid.uuid1())[:2])
-        archive_file_path = '{0}{1}-{2}.tar.gz'.format(cfg.FOLDER, prefix, cfg.PY_DB_MODULE)
-        print ('[i] making backup into file {0}'.format(archive_file_path))
+        prefix = "{0}-{1}".format(datetime.fromtimestamp(ts).strftime("%Y%m%d-%H%M%S"), str(uuid.uuid1())[:2])
+        archive_file_path = "{0}{1}-{2}.tar.gz".format(cfg.FOLDER, prefix, cfg.PY_DB_MODULE)
+        print("[i] making backup into file {0}".format(archive_file_path))
         tar = tarfile.open(archive_file_path, "w:gz")
         try:
-            files = ['{0}{1}.py'.format(cfg.FOLDER, cfg.PY_DB_MODULE), '{0}{1}.py'.format(cfg.FOLDER, cfg.PY_DB_MODULE_SKIPPED)]
+            files = [
+                "{0}{1}.py".format(cfg.FOLDER, cfg.PY_DB_MODULE),
+                "{0}{1}.py".format(cfg.FOLDER, cfg.PY_DB_MODULE_SKIPPED),
+            ]
             for file in files:
                 if os.path.exists(file):
                     tar.add(file)
         except:
-            print ('[i] nothing to backup')
+            print("[i] nothing to backup")
         tar.close()
 
-
     def read_unique_keys(self):
-        """ Reading unique data from CSV """
+        """Reading unique data from CSV"""
 
-        csv = open(cfg.DATA_FILE_PATH, 'r')
-        first_line = csv.readline() # ignoring first line
+        csv = open(cfg.DATA_FILE_PATH, "r")
+        first_line = csv.readline()  # ignoring first line
 
         results = {}
-        #f_uniques = {}
+        # f_uniques = {}
         for line in csv:
             rows = line.split(cfg.CSV_DELIMITER)
-            #f_uniques[rows[5]] = 1
-            if rows[5].upper() == 'SOLAR':
+            # f_uniques[rows[5]] = 1
+            if rows[5].upper() == "SOLAR":
                 results[rows[0]] = {
-                                    'address': (','.join((rows[1], rows[2], rows[3], rows[4])).replace('#', '1')),
-                                    'plz': rows[2],
-                                    'key': rows[0]
-                                   }
-        #print(f_uniques)
-        print ('[i] total unique keys {0}'.format(len(results)))
+                    "address": (",".join((rows[1], rows[2], rows[3], rows[4])).replace("#", "1")),
+                    "plz": rows[2],
+                    "key": rows[0],
+                }
+        # print(f_uniques)
+        print("[i] total unique keys {0}".format(len(results)))
         self.unique_keys = results
         return results
 
-
     def statistics(self):
-        """ Gathering statistics about already processed data """
+        """Gathering statistics about already processed data"""
 
         if len(self.geo_database) == 0:
             self.prepeare_data()
 
+        if self.geo_database is None or self.geo_database_skipped is None:
+            print("\n[e] cannot read the database backup, restart the script")
+            return False
+
         unique = len(self.unique_keys)
         properly_processed = len(self.geo_database)
         skipped_processed = len(self.geo_database_skipped)
-        print ('\n[i] already processed data {0}'.format(properly_processed + skipped_processed))
-        print ('[i] properly processed data {0}'.format(properly_processed))
-        print ('[i] skipped processed data {0}'.format(skipped_processed))
+
+        print("\n[i] already processed data {0}".format(properly_processed + skipped_processed))
+        print("[i] properly processed data {0}".format(properly_processed))
+        print("[i] skipped processed data {0}".format(skipped_processed))
         left_keys = unique - (properly_processed + skipped_processed)
         estimated_time_left = (left_keys / cfg.REQUESTS_PER_INTERVAL) / 60.0
-        print ('[i] more to go {0}'.format(left_keys))
-        print ('[i] estimated time in minutes {0}'.format(estimated_time_left))
-        print ('[i] estimated time in hours {0}\n'.format(estimated_time_left/60.0))
+        print("[i] more to go {0}".format(left_keys))
+        print("[i] estimated time in minutes {0}".format(estimated_time_left))
+        print("[i] estimated time in hours {0}\n".format(estimated_time_left / 60.0))
 
+        return True
 
     def get_coordinates(self, address):
-        """ Using geocoder to decode address
-            - can use various providers (Google, OSM, etc.) https://geocoder.readthedocs.org/
-            - can output GeoJSON
-            - proxies from http://www.proxy-listen.de/Proxy/Proxyliste.html
+        """Using geocoder to decode address
+        - can use various providers (Google, OSM, etc.) https://geocoder.readthedocs.org/
+        - can output GeoJSON
+        - proxies from http://www.proxy-listen.de/Proxy/Proxyliste.html
         """
 
-        #proxies = {'149.202.249.227:3128', '144.76.232.58:3128'}
+        # proxies = {'149.202.249.227:3128', '144.76.232.58:3128'}
 
-        #g_geocoder = geocoder.osm(address, proxies=proxies)
+        # g_geocoder = geocoder.osm(address, proxies=proxies)
         g_geocoder = geocoder.osm(address)
 
         # in case OSM didn't provide anything use Google Maps
@@ -134,12 +136,13 @@ class GeoConverter():
             g_geocoder = geocoder.arcgis(address)
 
         (latitude, longitude) = g_geocoder.latlng
-        if DEBUG: print ('[i] location : {0} {1}'.format(latitude, longitude))
+        if DEBUG:
+            print("[i] location : {0} {1}".format(latitude, longitude))
 
-        return {'lat': latitude, 'lng': longitude}
+        return {"lat": latitude, "lng": longitude}
 
     def proces_unique_keys(self):
-        """ Processing unique keys in order to retrieve coordinates"""
+        """Processing unique keys in order to retrieve coordinates"""
 
         if len(self.geo_database) == 0:
             self.prepeare_data()
@@ -151,21 +154,22 @@ class GeoConverter():
             cfg.save_data_as_pyobj(self.geo_database_skipped, cfg.PY_DB_MODULE_SKIPPED)
 
         for index, current_key in enumerate(self.unique_keys):
-            #if DEBUG: print ('[i] checking key {0}'.format(current_key))
-            if     current_key not in self.geo_database \
-               and current_key not in self.geo_database_skipped:
-                if DEBUG: print ('[i] not yet retrieved {0}'.format(current_key))
-                address = self.unique_keys[current_key]['address']
+            # if DEBUG: print ('[i] checking key {0}'.format(current_key))
+            if current_key not in self.geo_database and current_key not in self.geo_database_skipped:
+                if DEBUG:
+                    print("[i] not yet retrieved {0}".format(current_key))
+                address = self.unique_keys[current_key]["address"]
                 try:
                     retrived_location = self.get_coordinates(address)
                 except Exception as ex:
-                    if DEBUG: print ('[x] following key skipped {0}'.format(current_key))
-                    print ('[ex] Exception: {0}'.format(ex))
-                    self.geo_database_skipped[current_key] = {'key':current_key}
+                    if DEBUG:
+                        print("[x] following key skipped {0}".format(current_key))
+                    print("[ex] Exception: {0}".format(ex))
+                    self.geo_database_skipped[current_key] = {"key": current_key}
 
                 if current_key not in self.geo_database_skipped:
                     self.geo_database[current_key] = self.unique_keys[current_key]
-                    self.geo_database[current_key]['location'] = retrived_location
+                    self.geo_database[current_key]["location"] = retrived_location
 
                 if index % cfg.REQUESTS_PER_INTERVAL == 0:
                     sleep(cfg.SLEEP_INTERVAL)
@@ -175,39 +179,43 @@ class GeoConverter():
 
         save_data()
 
+
 class Logger(object):
 
     def __init__(self):
-        """ Initializing log file with random name"""
+        """Initializing log file with random name"""
 
         self.terminal = sys.stdout
-        suffix = '{0}-{1}'.format(datetime.fromtimestamp(time.time()).strftime('%Y%m%d-%H%M'), str(uuid.uuid1())[:2])
-        self.log = open('logfile-{0}.log'.format(suffix), "a")
+        suffix = "{0}-{1}".format(datetime.fromtimestamp(time.time()).strftime("%Y%m%d-%H%M"), str(uuid.uuid1())[:2])
+        self.log = open("logfile-{0}.log".format(suffix), "a")
 
     def write(self, message):
-        """ Overriding writing method to write to file and stdout at once"""
+        """Overriding writing method to write to file and stdout at once"""
 
         self.terminal.write(message)
         self.log.write(message)
 
     def flush(self):
         """flush method is needed for python 3 compatibility
-           handles the flush command by doing nothing
-           you might want to specify some extra behavior here
+        handles the flush command by doing nothing
+        you might want to specify some extra behavior here
         """
 
         pass
 
+
 def main():
-    """ Creating classes and initiating methods"""
+    """Creating classes and initiating methods"""
 
     geo_converter = GeoConverter()
-    geo_converter.statistics()
-    #geo_converter.make_db_backup()
+    if not geo_converter.statistics():
+        exit(1)
+
+    # geo_converter.make_db_backup()
     geo_converter.proces_unique_keys()
-    cfg.save_db_as_csv(delimiter=',')
+    cfg.save_db_as_csv(delimiter=",")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.stdout = Logger()
     main()
